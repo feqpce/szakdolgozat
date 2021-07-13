eec74369a39b97e251c9d4952258372aac9d2269
zsoltadam <zsadam@protonmail.com>
2021-07-13 21:14:21 +0200

    final pre showcase build

--- app/src/main/java/com/example/szakdolgozat/ui/cart/CartFragment.kt
+++ app/src/main/java/com/example/szakdolgozat/ui/cart/CartFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.biometric.BiometricPrompt
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
@@ -17,14 +18,13 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.szakdolgozat.databinding.FragmentCartListBinding
 import com.example.szakdolgozat.ui.IItemLongClickListener
 import com.google.mlkit.vision.barcode.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScanner
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
+import java.util.concurrent.Executor
 
 /**
  * A fragment representing a list of Items.
@@ -32,25 +32,63 @@ import com.google.mlkit.vision.barcode.BarcodeScanning
 class CartFragment : Fragment() {
     private val viewModel by viewModels<CartViewModel>()
     private lateinit var binding: FragmentCartListBinding
-    private var columnCount = 1
     private lateinit var scanner: BarcodeScanner
+
+    private val executor: Executor by lazy {
+        ContextCompat.getMainExecutor(context)
+    }
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
+        setUpBiometricPrompt()
 
+        setUpBarcodeScanner()
+
+        requestPermissionForCamera()
+    }
+
+    private fun setUpBiometricPrompt() {
+        biometricPrompt =
+            BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback() {
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+
+                    viewModel.checkout()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+
+                    Toast.makeText(context, "Authentication failed", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+
+                    Toast.makeText(context, "Authentication error: $errString", Toast.LENGTH_SHORT)
+                        .show()
+
+                }
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Checkout")
+            .setSubtitle("Confirm your identity to pay")
+            .setNegativeButtonText("Cancel")
+            .build()
+    }
+
+    private fun setUpBarcodeScanner() {
         val options = BarcodeScannerOptions.Builder()
             .setBarcodeFormats(
                 Barcode.FORMAT_QR_CODE
             )
             .build()
         scanner = BarcodeScanning.getClient(options)
-
-        requestPermissionForCamera()
-
     }
 
     override fun onCreateView(
@@ -59,22 +97,22 @@ class CartFragment : Fragment() {
     ): View {
 
         binding = FragmentCartListBinding.inflate(inflater, container, false)
-        binding.lifecycleOwner = viewLifecycleOwner
+
         val listAdapter = ProductRecyclerViewAdapter(object : IItemLongClickListener<Product> {
             override fun onItemLongClick(item: Product) {
                 viewModel.removeProductFromCart(item.id)
             }
         })
 
-        // Set the adapter
-        with(binding.list) {
-            layoutManager = when {
-                columnCount <= 1 -> LinearLayoutManager(context)
-                else -> GridLayoutManager(context, columnCount)
+        with(binding) {
+            lifecycleOwner = viewLifecycleOwner
+            list.adapter = listAdapter
+            model = viewModel
+            checkoutButton.setOnClickListener {
+                biometricPrompt.authenticate(promptInfo)
             }
-            adapter = listAdapter
         }
-        binding.model = viewModel
+
         viewModel.cart.observe(viewLifecycleOwner) {
             Log.d("PRODUCTS", it.toString())
             listAdapter.submitList(it)
@@ -91,7 +129,7 @@ class CartFragment : Fragment() {
         // Register the permissions callback, which handles the user's response to the
         // system permissions dialog. Save the return value, an instance of
         // ActivityResultLauncher. You can use either a val, as shown in this snippet,
-        // or a lateinit var in your onAttach() or onCreate() method.
+        // or a late init var in your onAttach() or onCreate() method.
         val requestPermissionLauncher =
             registerForActivityResult(
                 ActivityResultContracts.RequestPermission()
@@ -100,13 +138,7 @@ class CartFragment : Fragment() {
                     // Permission is granted. Continue the action or workflow in your
                     // app.
                     setupCamera(scanner)
-                } //else {
-                // Explain to the user that the feature is unavailable because the
-                // features requires a permission that the user has denied. At the
-                // same time, respect the user's decision. Don't link to system
-                // settings in an effort to convince the user to change their
-                // decision.
-                //}
+                }
             }
         when {
             ContextCompat.checkSelfPermission(
@@ -172,20 +204,5 @@ class CartFragment : Fragment() {
             )
 
         }, executor)
-    }
-
-    companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            CartFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
     }
 }
